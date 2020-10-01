c3d7cd912f0201f51f34623ea63e6deb5b33df06
NFCxNS <72204691+NFCxNS@users.noreply.github.com>
2020-10-01 14:30:00 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CCH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBTC = new System.Windows.Forms.ComboBox();
+            this.textBTC = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.Listener = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insert your bitcon address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select address format:";
+            // 
+            // comboBTC
+            // 
+            this.comboBTC.FormattingEnabled = true;
+            this.comboBTC.Items.AddRange(new object[] {
+            "P2PKH",
+            "P2SH",
+            "Bech32"});
+            this.comboBTC.Location = new System.Drawing.Point(149, 66);
+            this.comboBTC.Name = "comboBTC";
+            this.comboBTC.Size = new System.Drawing.Size(121, 21);
+            this.comboBTC.TabIndex = 2;
+            // 
+            // textBTC
+            // 
+            this.textBTC.Location = new System.Drawing.Point(149, 26);
+            this.textBTC.Name = "textBTC";
+            this.textBTC.Size = new System.Drawing.Size(200, 20);
+            this.textBTC.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(78, 117);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 30);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(159, 117);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 30);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(240, 117);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 30);
+            this.btnQuit.TabIndex = 6;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // Listener
+            // 
+            this.Listener.Tick += new System.EventHandler(this.Get_Clipboard);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBTC);
+            this.Controls.Add(this.comboBTC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CCH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBTC;
+        private System.Windows.Forms.TextBox textBTC;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Timer Listener;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Non-Ethic Soft
+ * Author: NFC
+*/
+
+namespace CCH
+{
+    public partial class Form1 : Form
+    {
+        Regex btcaddr = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Regex BTC_Format()
+        {
+            Regex re = null;
+
+            if (comboBTC.Text == "P2PKH")
+            {
+                re = new Regex("^[1][a-zA-Z0-9]{26,35}$");
+            }
+            else if (comboBTC.Text == "P2SH")
+            {
+                re = new Regex("^[3][a-zA-Z0-9]{26,35}$");
+            }
+            else if (comboBTC.Text == "Bech32")
+            {
+                re = new Regex("^(bc1)([a-z0-9]){38,62}$");
+            }
+            return re;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (Listener.Enabled)
+            {
+                MessageBox.Show("Program is already running!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; 
+            }
+
+            string address = textBTC.Text.Trim();
+            
+            if (String.IsNullOrEmpty(address))
+            {
+                MessageBox.Show("Address field is empty!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                btcaddr = BTC_Format();
+
+                if (btcaddr != null)
+                {
+                    if (!btcaddr.IsMatch(address))
+                    {
+                        MessageBox.Show("Wrong input!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBTC.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Clipboard is monitored!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Listener.Start();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You haven't selected address format!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void Swap()
+        {
+            try
+            {
+                Clipboard.SetDataObject(textBTC.Text, false, 0, 0);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void Get_Clipboard(object sender, EventArgs e)
+        {
+            string copied = Clipboard.GetText().Trim();
+
+            if (copied.Length > 0)
+            {
+                if (btcaddr.IsMatch(copied))
+                    Swap();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (Listener.Enabled)
+            {
+                MessageBox.Show("Monitoring stopped!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Listener.Stop();
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+    }
+}
